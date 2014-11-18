79172c6a5bad3fd3e468e131b4565a0c1998d9e5
brentzucker <brentzucker@gmail.com>
2014-11-18 16:02:27 -0500

    GameObject created, Modified Question.java too
    Question.java - added compareTo to allow them to be added in a set
    
    GameObject.java - reads text file to build question library arrayList,
    generates a random set of questions, puts set into stack so questions
    can by popped in synchronization with server and client
    question
    
    question_library.txt -  originally question_set.txt, 12 questions

--- TriviaGameServer/src/Question.java
+++ TriviaGameServer/src/Question.java
@@ -1,58 +1,8 @@
 
-public class Question {
-	
-	/*
-	public String question;
-	private String answer_A;
-	private String answer_B;
-	private String answer_C;
-	private String answer_D;
-	private char correct_answer;
-	
-	
-	public Question(String[] question_answer_array, char correct_answer){
-		
-		this.question = question_answer_array[0];
-		this.answer_A = question_answer_array[1];
-		this.answer_B = question_answer_array[2];
-		this.answer_C = question_answer_array[3];
-		this.answer_D = question_answer_array[4];
-		
-		if(correct_answer == 'A' || correct_answer == 'B' || correct_answer == 'C' || correct_answer == 'D'){
-			this.correct_answer = correct_answer;
-		}
-		else
-			throw new RuntimeException("Invalid Answer");
-		
-	}
-	
-	
-	public boolean isCorrect(char answer){
-		return answer == this.correct_answer;
-	}
-	
-	public String getQuestion(){
-		return question;
-	}
-	
-	public String getAnswerA(){
-		return answer_A;
-	}
-	
-	public String getAnswerB(){
-		return answer_B;
-	}
-	
-	public String getAnswerC(){
-		return answer_C;
-	}
-	
-	public String getAnswerD(){
-		return answer_D;
-	}
-	*/
+public class Question implements Comparable<Question> 
+{
 
-	/*
+/*
  * Question Objects - can be created from a file reader
  *
 */
@@ -123,5 +73,17 @@ public class Question {
 		}
 		else return false;
 	}
+
+	 public int compareTo(Question q)
+ 	{
+ 		final int BEFORE = -1;
+    	final int EQUAL = 0;
+    	final int AFTER = 1;
+
+    	if(question.equals(q.question))
+    		return EQUAL;
+
+    	return AFTER;
+ 	}
 
 }
